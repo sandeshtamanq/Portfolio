3aed36af0eebab841fbb0c6b29781044525c5fb9
codeinvisible <lamasandesh400@gmail.com>
2023-02-23 19:16:10 +0545

    beta

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -3,15 +3,11 @@ import React from "react";
 const Welcome: React.FC = () => {
   return (
     <>
-      <div className="pb-10">Welcome to my terminal portfolio. (Version 0.0.1)</div>
+      <div className="pb-10">Welcome to my terminal portfolio.(Beta)</div>
       <div className="text-red-500 pb-10">
         Type <span className="text-yellow-500 y-10">`help`</span> into the terminal and hit enter to see lists of all the commands
       </div>
-      <div className="pb-10">
-        ---------------------------------
-        <p>This is still in development</p>
-        ---------------------------------
-      </div>
+      <div className="pb-10"></div>
     </>
   );
 };
